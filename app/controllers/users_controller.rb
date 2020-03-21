f0bd1e1cd5ffa20cc599992c3b181d606704bf1b
ngocson <ngocson13btt@gmail.com>
2020-03-21 23:05:24 +0700

    add gem bcrypt && sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:succes] = "Sign Up Success"
+      redirect_to root_path
+    else
+      flash[:succes] = "Registration Failed"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :email, :fullname, :password
+  end
 end
