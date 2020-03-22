3036ac8b1ea254ff09e5ec9f4058d99a788cf6c6
ngocson <ngocson13btt@gmail.com>
2020-03-22 22:00:10 +0700

    logout && show

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,37 @@
+# frozen_string_literal: true
+
 module SessionsHelper
   def sign_in(user)
-   session[:user_id] = user.id
+    session[:user_id] = user.id
  end
 
- def remember(user)
-   user.remember
-   cookies.permanent.signed[:user_id] = user.id
-   cookies.permanent[:remember_token] = user.remember_token
+  def logout
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find(user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find(user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        login user
+        @current_user = user
+      end
+    end
  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post "users/sign_up" => "users#create"
   get "users/sign_in" => "sessions#new"
   post "users/sign_in" => "sessions#create"
+  get 'users/id' => 'users#show', as: :user
+  delete 'logout' => 'sessions#destroy'
 end
