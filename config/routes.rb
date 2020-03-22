823c0d8b34882cd9a3413591458512a63b9c68e2
ngocson <ngocson13btt@gmail.com>
2020-03-22 20:46:47 +0700

    add remember me && register

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get "users/sign_up" => "users#new"
   post "users/sign_up" => "users#create"
+  get "users/sign_in" => "sessions#new"
+  post "users/sign_in" => "sessions#create"
 end
