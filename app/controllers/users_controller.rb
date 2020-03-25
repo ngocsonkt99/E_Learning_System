bddc05aba1b70303b42a3f5fdffd9793a0c26978
ngocson <ngocson13btt@gmail.com>
2020-03-25 16:15:35 +0700

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      UserMailer.welcome_email(@user).deliver_now
       flash[:notice] = "Sign Up Success"
       redirect_to root_path
     else
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,17 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain:  'example.com',
+    user_name: '<username>',
+    password:  '<password>',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
