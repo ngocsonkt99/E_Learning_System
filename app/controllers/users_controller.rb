df4655a47782d9b8b5890189dec9fd2b663bd049
ngocson <ngocson13btt@gmail.com>
2020-03-29 21:13:43 +0700

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      UserMailer.with(user: @user).welcome_email.deliver_now
+#      UserMailer.with(user: @user).welcome_email.deliver_now
       flash[:notice] = 'Sign Up Success'
       redirect_to root_path
     else
